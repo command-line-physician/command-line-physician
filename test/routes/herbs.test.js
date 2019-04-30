c1e310188d61b823aa33f047224a97f184865219
Mallarie <mallariemilano@gmail.com>
2019-04-30 11:24:14 -0700

    get a list of allllll herbs passing

--- test/routes/herbs.test.js
+++ test/routes/herbs.test.js
@@ -2,6 +2,7 @@ const app = require('../../lib/app');
 const request = require('supertest');
 const mongoose = require('mongoose');
 const User = require('../../lib/models/userSchema');
+
 describe('herb routes', () => {
   beforeAll(() => {
     return mongoose.connect('mongodb://localhost:27017/herbs', {
@@ -57,6 +58,42 @@ describe('herb routes', () => {
               user: expect.any(String),
               __v: 0
             });
+          });
+      });
+  });
+  it('can return a list of all herbs', () => {
+    let token = null;
+    let userId = null;
+
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'intromode@email.com',
+        password: 'youllneverguess',
+        profilePhoto: 'coolPhoto.jpg'
+      })
+      .then(res => {
+        token = res.body.token;
+        userId = res.body.user._id;
+
+        return request(app)
+          .post('/api/v1/herbs')
+          .set('Authorization', `Bearer ${token}`)
+          .send({
+            name: 'lobelia',
+            category: 'nervine & antispasmodic',
+            latin_name: 'lobelia inflata',
+            medicinal_uses: 'cures everything',
+            description: 'is a smart herb',
+            user: userId
+            
+          })
+          .then(() => {
+            return request(app)
+              .get('/api/v1/herbs');
+          })
+          .then(res => {
+            expect(res.body).toHaveLength(1);
           });
       });
   });

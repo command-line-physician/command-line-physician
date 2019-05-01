e594970f049d15461283d14c96ec5cc94b4a6cb2
Mallarie <mallariemilano@gmail.com>
2019-04-30 17:22:58 -0700

    finding faves by ID test passing

--- test/routes/faves.test.js
+++ test/routes/faves.test.js
@@ -67,4 +67,69 @@ describe('faves routes', () => {
         });
       });
   });
+  it('can get a favorite by id', () => {
+    let token = null;
+    let userId = null;
+
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'intromode@email.com',
+        password: 'youllneverguess',
+        profilePhoto: 'coolPhoto.jpg'
+      })
+      .then(res => {
+        token = res.body.token;
+        userId = res.body.user._id;
+
+        return Promise.all([request(app)
+          .post('/api/v1/herbs')
+          .set('Authorization', `Bearer ${token}`)
+          .send({
+            name: 'lobelia',
+            category: 'nervine & antispasmodic',
+            latin_name: 'lobelia inflata',
+            medicinal_uses: 'cures everything',
+            description: 'is a smart herb',
+            user: userId
+          }), request(app)
+          .post('/api/v1/herbs')
+          .set('Authorization', `Bearer ${token}`)
+          .send({
+            name: 'lobeliaaaaa',
+            category: 'nervine & antispasmodic',
+            latin_name: 'lobelia inflata',
+            medicinal_uses: 'cures everything',
+            description: 'is a smart herb',
+            user: userId
+          })
+        ]);
+      })
+      .then(([herb1, herb2]) => {
+        return Promise.all([
+          request(app)
+            .post('/api/v1/faves')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+              herb: herb1.body._id,
+              user: userId
+            }), request(app)
+            .post('/api/v1/faves')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+              herb: herb2.body._id,
+              user: userId
+            })
+        ]);
+      })
+      .then(() => {
+        return request(app)
+          .get(`/api/v1/faves/${userId}`)
+          .set('Authorization', `Bearer ${token}`);
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(2);
+      });
+
+  });
 });
